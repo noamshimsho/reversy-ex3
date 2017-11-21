6f449fc53caf56627df1b3aa80f8249979b1454b
noam shimshoviz <noamshimsho@gmail.com>
2017-11-21 13:52:05 +0200

    new main

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,32 @@
 
 
 #include "GameFlow.h"
-
+#include "Player.h"
+#include "HumanPlayer.h"
 
 int main() {
 
+	Player* p1 = new HumanPlayer ('x');
+	Player* p2 = new HumanPlayer ('0');
 
-	GameFlow game(8,8);
-	game.Play();
-	cout << "git!!"
+	Board b(8,8);
+
+
+	GameLogic logic(b);
+
+	Board u =  logic.getBoard();
+
+	u.print();
+	b.print();
+
+	u.update(3,4,'x');
+	b.update(1,1,'0');
+
+
+
+
+delete p1;
+delete p2;
 
 
 

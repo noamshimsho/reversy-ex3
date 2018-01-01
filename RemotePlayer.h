ce833ad9739fc893a0d46ec70f6a366af6e37a8d
Sarit Zevin <saritzevin@gmail.com>
2018-01-01 15:09:09 +0200

    update

--- RemotePlayer.h
+++ RemotePlayer.h
@@ -1,5 +1,5 @@
 //
-// Created by sarit on 28/12/17.
+//      Author: Noam shimshoviz 203565429 Sarit Zevin 313242588
 //
 
 #ifndef UNTITLED_REMOTEPLAYER_H
@@ -12,12 +12,12 @@ class RemotePlayer: public Player {
 
 public:
     /**
-    * constructor HumanPlayer calling to the base c'tor of player.
+    * constructor RemotePlayer calling to the base c'tor of player.
     * @param name, the player tag for the game
     */
     RemotePlayer(discSymbol name);
     /**
-    * the HumanPlayer play his turn
+    * the RemotePlayer play his turn
     * @param logic, the rules of the game
     * @param moves- vector of possible moves
     */
